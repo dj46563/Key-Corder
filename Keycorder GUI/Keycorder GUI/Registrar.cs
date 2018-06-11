d2fae2bbdb6300fd8eecdb303405a431f60a6650
Tyler Page <tyler.james.page98@gmail.com>
2018-06-11 15:59:28 -0700

    Added summary to the output

--- Keycorder GUI/Keycorder GUI/Registrar.cs
+++ Keycorder GUI/Keycorder GUI/Registrar.cs
@@ -2,8 +2,10 @@
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -119,6 +121,10 @@ namespace Keycorder_GUI
 
         public void OutputToSheet(string filename)
         {
+            // Creating the summary lists
+            var pressStats = Enumerable.Repeat(new { Key = Key.A, Count = 0 }, 0).ToList();
+            var durStats = Enumerable.Repeat(new { Key = Key.A, Time = TimeSpan.Zero }, 0).ToList();
+
             using (ExcelPackage package = new ExcelPackage())
             {
                 // Create workbook and headers
@@ -150,7 +156,65 @@ namespace Keycorder_GUI
                     {
                         ws.Cells[row, 4].Value = keyEvent.End.ToString(@"mm\:ss\:ff");
                         ws.Cells[row, 5].Value = keyEvent.Duration.ToString(@"mm\:ss\:ff");
+
+                        // add dur event to durStats
+                        var stat = durStats.Find(x => x.Key == keyEvent.Key);
+                        if (durStats.Select(x => x.Key).Contains(keyEvent.Key))
+                        {
+                            TimeSpan time = stat.Time;
+                            durStats[durStats.FindIndex(x => x.Key == keyEvent.Key)] =
+                                new {Key = keyEvent.Key, Time = time.Add(keyEvent.Duration)};
+                        }
+                        else
+                        {
+                            durStats.Add(new {Key = keyEvent.Key, Time = keyEvent.Duration });
+                        }
                     }
+                    else
+                    {
+                        // add press event to pressStats
+                        var stat = pressStats.Find(x => x.Key == keyEvent.Key);
+                        if (pressStats.Select(x => x.Key).Contains(keyEvent.Key))
+                        {
+                            int count = stat.Count;
+                            pressStats[pressStats.FindIndex(x => x.Key == keyEvent.Key)] =
+                                new {Key = keyEvent.Key, Count = count + 1};
+                        }
+                        else
+                        {
+                            pressStats.Add(new { Key = keyEvent.Key, Count = 1 });
+                        }
+                    }
+                    row++;
+                }
+
+                // Output the summary info
+                ws.Cells["G1"].Value = "Summary";
+
+                // display press stats
+                ws.Cells["G2"].Value = "Key";
+                ws.Cells["H2"].Value = "Behavior";
+                ws.Cells["I2"].Value = "Count";
+                row = 3;
+                foreach (var stat in pressStats)
+                {
+                    ws.Cells[row, 7].Value = stat.Key;
+                    ws.Cells[row, 8].Value = GetBehaviorOfKey(stat.Key);
+                    ws.Cells[row, 9].Value = stat.Count;
+                    row++;
+                }
+
+                // display dur stats
+                row += 2;
+                ws.Cells[row, 7].Value = "Key";
+                ws.Cells[row, 8].Value = "Behavior";
+                ws.Cells[row, 9].Value = "Percentage";
+                row++;
+                foreach (var stat in durStats)
+                {
+                    ws.Cells[row, 7].Value = stat.Key;
+                    ws.Cells[row, 8].Value = GetBehaviorOfKey(stat.Key);
+                    ws.Cells[row, 9].Value = Math.Round((stat.Time.TotalMilliseconds / _stopwatch.Elapsed.TotalMilliseconds * 100), 3).ToString(CultureInfo.InvariantCulture) + "%";
                     row++;
                 }
 
